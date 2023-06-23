f2dc946f5006ea52ecb92171830b35277595e4ff
tomas <tomas153lm@gmail.com>
2023-06-23 15:31:47 -0300

    feat: upgrade 1

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 var pontos = 0;
+var isUpgrade1On = false;
+var multiplicador1 = 0;
 
-function pontuar() {
-    pontos++;
+function pontuar(num) {
+    
     const qtd = document.getElementById("qtd");
     qtd.innerHTML = "Maças: " + pontos;
+    switch(num) {
+        case 1:
+            pontos++;
+        break;
+        case 2:
+            pontos = pontos * multiplicador1;
+        break;
+    }
 }
+
+function upgrade(num) {
+    switch(num) {
+        case 1:
+            isUpgrade1On = true;
+            multiplicador1++;
+        break;
+
+    }
+}
+
+setInterval(() => {
+    if(isUpgrade1On) {
+        pontuar(2);
+    }
+}, 5000);
