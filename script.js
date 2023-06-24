bf54a96b42f7a8fd2047e051679610675b268aef
tomas <tomas153lm@gmail.com>
2023-06-24 11:10:55 -0300

    feat: upgrade 1 funcionando

--- script.js
+++ script.js
@@ -2,33 +2,41 @@
 let getCookie = document.cookie.split(";");
 var pontos = getCookie[0] == '' ? 0 : parseInt(getCookie[0]);
 const qtd = document.getElementById("qtd");
-qtd.innerHTML = "Maças: " + pontos;
+qtd.innerHTML = "Maças: " + parseInt(pontos);
 // Declaração de upgrades
+var isUpgrade1On = false;
 var precoUpgrade1 = 50;
-var multiplicador1 = 0;
+var multiplicador1 = 1;
 var isUpgrade2On = false;
 var multiplicador2 = 0;
 
 function pontuar(num) {
     switch(num) {
         case 1:
-            pontos = pontos+1+(1*multiplicador1);
+            if(isUpgrade1On) {
+                pontos += 1*multiplicador1;
+            } else {
+                pontos++;
+            }
         break;
         case 2:
             pontos = pontos * multiplicador2;
         break;
     }
     const qtd = document.getElementById("qtd");
-    qtd.innerHTML = "Maças: " + pontos;
+    qtd.innerHTML = "Maças: " + parseInt(pontos);
 }
 
 function upgrade(num) {
     switch(num) {
         case 1:
             if(pontos >= precoUpgrade1) {
+                isUpgrade1On = true;
                 pontos-=precoUpgrade1;
                 precoUpgrade1 = precoUpgrade1*1.75;
                 multiplicador1++;
+                const qtd = document.getElementById("qtd");
+                qtd.innerHTML = "Maças: " + parseInt(pontos);
             } else {
                 document.getElementById("erro").style.display = "flex";
             }
@@ -39,11 +47,11 @@ function upgrade(num) {
     }
 }
 
-setInterval(() => {
-    if(isUpgrade2On) {
-        pontuar(2);
-    }
-}, 5000);
+// setInterval(() => {
+//     if(isUpgrade2On) {
+//         pontuar(2);
+//     }
+// }, 5000);
 
 function salvar() {
     let cookieString = pontos;
